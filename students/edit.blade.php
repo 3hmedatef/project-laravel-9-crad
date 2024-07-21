cda946d27b464f2f7e144abb32ec1cf2dc390a68
3hmedatef <159903049+3hmedatef@users.noreply.github.com>
2024-07-21 13:01:20 +0300

    Add files via upload

---
+++ students/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('students.layout')
+@section('content')
+ 
+<div class="card">
+  <div class="card-header">Edite Page</div>
+  <div class="card-body">
+      
+      <form action="{{ url('student/' .$students->id) }}" method="post">
+        {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
+        <label>password</label></br>
+        <input type="password" name="password" id="password" value="{{$students->password}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
+    </form>
+   
+  </div>
+</div>
+ 
+@stop
